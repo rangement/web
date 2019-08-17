46db8c05df46d379b3bdf7a3cc74245d926f4d50
testerbot <thefly800@gmail.com>
2019-08-17 16:48:12 +0200

    Date Script --> Internal

--- desktop-script.js
+++ desktop-script.js
@@ -22,21 +22,6 @@
 
 	});
 
-
-
-  	document.getElementById("para1").innerHTML = formatAMPM();
-
-  	function formatAMPM() {
-    let d = new Date(),
-      minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
-      hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
-      ampm = d.getHours() >= 12 ? 'pm' : 'am',
-      months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
-      days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-      at = "at";
-      return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+', '+hours+':'+minutes+ampm;
-  	}
-
 // }
 
 //MOBILE SCRIPT ____________________________________________________________
