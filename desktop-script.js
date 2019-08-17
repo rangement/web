812b97ea991fc11e4b856ee6347565e0b348b2d5
testerbot <thefly800@gmail.com>
2019-08-17 18:11:50 +0200

    Mobile Check

--- desktop-script.js
+++ desktop-script.js
@@ -1,6 +1,6 @@
 //DESKTOP SCRIPT
 
-// if($(window).width() > 500){
+if (screen.width >= 450) {
 
 	var show = 0;
 	var timeset;
@@ -22,33 +22,41 @@
 
 	});
 
-// }
+	console.log("DESKTOP");
+
+}
 
 //MOBILE SCRIPT ____________________________________________________________
 
+else {
 
-if($(window).width() < 400){
+	$(window).load(function() {
 
-	var show = 0;
-	var timeset;
-	
-	$( window ).mousemove(function( event ) {
-		window.clearTimeout(timeset);
-		$('.item').mouseenter(function(){
-			show = $(this).attr('alt').replace(/\D/g,''); 
-			// /\D/g = global search for non-digit characters
+		$('html').css('background-color','lightgrey');
+		$('header').css('background-color','lightgrey');
+		$('footer').css('background-color','lightgrey');
+		$('main').css('background-color','lightgrey');
+		$('.container-fluid').css('background-color','lightgrey');
+		$('.item-image').css({"align-items":"center","display":"none"});
+
+		$(document).ready(function () {
+    		$(".item").click(function () {
+        	$(this).next('div.item-image').slideToggle("slow");
+    		});
+
 		});
+
 		
-		var showDiv = 'item-image'+ show;
-
-		$('.item-image').css('display','none');
-		$('.item-image'+ show).css('display','flex');
-		timeset = setTimeout(function(){ 
-			$('.item-image').css('display','none'); 
-		}, 1500);
-
 	});
 
-window.alert("400px Breakpoint Reached!")
+
+	console.log("MOBILE");
+
 
 }
+
+
+// ("position":"fixed","align-items":"center","display":"flex", "justify-content":"center","position":"fixed");
+
+
+	
