222b627650526deab39cfd9bdae7a0acf62ec8f7
testerbot <thefly800@gmail.com>
2019-08-18 15:48:40 +0200

    added scetion-2

--- desktop-script.js
+++ desktop-script.js
@@ -22,6 +22,93 @@ if (screen.width >= 450) {
 
 	});
 
+
+
+
+		$( window ).mousemove(function( event ) {
+		window.clearTimeout(timeset);
+		$('.item').mouseenter(function(){
+			show = $(this).attr('alt').replace(/\D/g,''); 
+			// /\D/g = global search for non-digit characters
+		});
+		
+		var showDiv = 'item-image'+ show;
+
+		$('.item-image').css('display','none');
+		$('.item-image'+ show).css('display','flex');
+		timeset = setTimeout(function(){ 
+			$('.item-image').css('display','none'); 
+		}, 2000);
+
+	});
+
+
+//opens up section-2 on item click:
+
+	$(function () {
+	  $('.item').click(function () {
+        $('#section-2').toggleClass('active');
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+        return true;
+      })
+    })
+
+
+//close section-2 when scroll back to top:
+
+	window.onscroll = function(e) {	
+		var bod = document.body; 
+    	var doc = document.documentElement; 
+    	doc = (doc.clientHeight)? doc : bod;
+	
+		if (doc.scrollTop == 0){
+			if ($('#section-2').hasClass('active')){
+		 		$('#section-2').removeClass('active');
+			}  
+		};      
+	};
+
+//scroll to bottom onclick of "Bottom"
+//scroll to top onclick of "Top"
+
+	$(function () {
+	  $('#lang').click(function () {
+  		$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+  		$("#lang-2").css('display','inline');
+  		$("#lang").css('display','none');
+  	  })
+	})
+
+	$(function () {
+	  $('#lang').click(function () {
+	  	$("html, body").animate({scrollTop: 0}, 500);
+  		$("#lang").css('display','inline');
+  		$("#lang-2").css('display','none');
+  	  })
+	})
+
+
+// When the user scrolls down 100px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+  		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+    		document.getElementById("lang-2").style.display = "inline";
+    		document.getElementById("lang").style.display = "none";
+  		} else {
+    		document.getElementById("lang").style.display = "inline";
+    		document.getElementById("lang-2").style.display = "none";
+  		}
+	}
+	
+// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+  		document.body.scrollTop = 0;
+  		document.documentElement.scrollTop = 0;
+	}	
+
+
+// signals that this is script for Desktop media size
 	console.log("DESKTOP");
 
 }
@@ -31,26 +118,10 @@ if (screen.width >= 450) {
 else {
 
 	$(window).load(function() {
-
-		$('html,.container-fluid,header,footer,main').css('background-color','lightgrey');
-		$('a.item').click(function(e) {
-    		e.preventDefault();
-    		//do other stuff when a click happens
-		});
-		
-		$(document).ready(function () {
-    		$("a.item").click(function () {
-        	$(this).next('div.item-image').slideToggle("slow");
-    		});
-
-		});
-
-		
+		$('html,.container-fluid,header,footer,main').css('background-color','lightgrey');	
 	});
 
-
 	console.log("MOBILE");
-
 
 }
 
